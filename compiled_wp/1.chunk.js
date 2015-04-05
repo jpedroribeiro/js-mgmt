69bf79d3ae3bde8db2c84580b210e16bfff9e18d
J. Pedro Ribeiro <jpedroribeiro@joaopar-air.local>
2015-04-05 16:43:40 +0100

    Added webpack server

--- compiled_wp/1.chunk.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([1],{2:function(n,o,e){var t;t=function(){console.log("This is lazy loaded")}.call(o,e,o,n),!(void 0!==t&&(n.exports=t))}});
-//# sourceMappingURL=1.chunk.js.map
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
         path: "compiled_wp",
         filename: "[name].js",
         chunkFilename: "[name].[id].js", // TODO chunk name?
-        publicPath: "/compiled_wp/"
+        //publicPath: "/compiled_wp/"
+        publicPath: "http://localhost:8080/compiled_wp/"
     },
     resolve: {
         root: "./js_wp"
