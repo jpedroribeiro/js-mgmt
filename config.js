077ec3bd9ee811eb0e72ff6820465c2d7119707a
J. Pedro Ribeiro <jpedroribeiro@joaopar-air.local>
2015-04-06 16:09:11 +0100

    jspm: dev mode only, async OK

--- config.js
+++ config.js
@@ -10,16 +10,6 @@ System.config({
     "*": "*.js",
     "github:*": "jspm_packages/github/*.js",
     "npm:*": "jspm_packages/npm/*.js"
-  },
-  "bundles": {
-    "build": [
-      "js_jspm/myModule",
-      "js_jspm/main"
-    ],
-    "compiled_jspm/main": [
-      "js_jspm/myModule",
-      "js_jspm/main"
-    ]
   }
 });
 
@@ -27,7 +17,6 @@ System.config({
   "map": {
     "babel": "npm:babel@4.7.16",
     "babel-runtime": "npm:babel-runtime@4.7.16",
-    "jquery": "github:components/jquery@2.1.3",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
--- js_jspm/main.js
+++ js_jspm/main.js
@@ -1,13 +1,13 @@
 import {myModule} from './myModule';
-import $ from 'jquery';
 myModule.init();
 
 // async, this works as expected 
-//$('body').on('click', function(){
-	System.import('./js_wp/asyncModule').then(function(){
+$('body').on('click', function(){
+	System.import('./js_jspm/asyncModule').then(function(myModule){
+		myModule.asyncModule();
 		console.log("after async");
 	});
-//});
+});
 
 
 
